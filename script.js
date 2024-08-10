49ff454324fc18302225c44bea85bfd557209411
rambabupathakamuri <rpathakamuri@rivier.edu>
2024-08-10 15:20:06 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Initialize a simple map placeholder
+    const map = document.getElementById('map');
+    map.innerHTML = '<p>Map placeholder: 123 Flavor Street, Boston, MA</p>';
+
+    // Additional JavaScript functionality can be added here
+});
